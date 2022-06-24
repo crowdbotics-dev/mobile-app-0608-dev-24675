b88f3340bf0c236f48dafae9da23a856af264a20
Aline Tavares <aline@crowdbotics.com>
2022-06-24 10:30:24 -0400

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -1,3 +1,5 @@
+import { Text } from "react-native";
+import { FlatList } from "react-native";
 import React from "react";
 import { View } from "react-native";
 
@@ -8,7 +10,35 @@ const Untitled1 = () => {
     position: 'relative',
     height: '100%'
   }}>
-      </View>;
+      <FlatList renderItem={() => <View style={{
+      minHeight: 60,
+      backgroundColor: "#ddd9e1",
+      borderRadius: "50%",
+      left: 0,
+      top: 0,
+      width: 151,
+      height: 60,
+      borderWidth: 1
+    }}><Text style={{
+        left: 26,
+        top: 11,
+        position: "absolute",
+        width: 99,
+        height: 32,
+        lineHeight: 14,
+        fontSize: 14,
+        borderRadius: 0,
+        color: "#c22020"
+      }}>Hello!</Text></View>} data={[1, 2, 3, 4]} keyExtractor={item => item.toString()} style={{
+      left: 14,
+      top: 27,
+      position: "absolute",
+      width: 151,
+      height: 200
+    }} ItemSeparatorComponent={() => <View style={{
+      backgroundColor: '#000000',
+      height: 1
+    }} />}></FlatList></View>;
 };
 
 export default Untitled1;
