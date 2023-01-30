62be8ed78e06a0c3e8c39b70719e268fc3d58bca
crowdbotics-dev <engineering@crowdbotics.com>
2023-01-30 15:26:04 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -7,3 +7,4 @@ class Pet(models.Model):
     sex = models.CharField(max_length=100,)
     created_at = models.DateTimeField(auto_now_add=True,)
     updated_at = models.DateTimeField(auto_now=True,)
+    adopted = models.BooleanField(null=True,blank=True,)
